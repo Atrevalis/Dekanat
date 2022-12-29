9ec6095d64ec547d23d01b9b50f253ec2d48f849
atrevalis <atrevalis@mail.ru>
2022-12-29 18:58:22 +0300

    minusbroken autocode

--- src/main/java/GUI/LoginForm.java
+++ src/main/java/GUI/LoginForm.java
@@ -36,6 +36,4 @@ public class LoginForm {
         loginButton.addActionListener(loginListener);
 
     }
-
-
 }
--- src/main/java/GUI/StudentsList.java
+++ src/main/java/GUI/StudentsList.java
@@ -1,10 +1,12 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class StudentsList {
     private JButton button1;
     private JButton button2;
     private JButton button3;
     private JButton button4;
+
 }
